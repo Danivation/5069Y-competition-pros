8436de52c3cdacc05886c0c73aba466e1ad43679
Daniel Velasco <divelasco7@gmail.com>
2024-12-15 19:10:40 -0600

    inertial

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,7 @@
 #include "main.h"
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button()
-{
-  static bool pressed = false;
-  pressed = !pressed;
-  if (pressed)
-  {
-    pros::lcd::set_text(2, "I was pressed!");
-  }
-  else
-  {
-    pros::lcd::clear_line(2);
-  }
-}
+pros::Controller Controller(CONTROLLER_MASTER);
+pros::Imu Inertial(1);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -28,20 +11,7 @@ void on_center_button()
  */
 void initialize()
 {
-  pros::lcd::initialize();
-  pros::lcd::set_text(1, "Hello PROS User!");
-
-  pros::lcd::register_btn1_cb(on_center_button);
-}
-
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled()
-{
-
+  Inertial.reset(true); // calibrate the inertial sensor and wait until it finishes
 }
 
 /**
@@ -54,6 +24,16 @@ void disabled()
  * starts.
  */
 void competition_initialize()
+{
+
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled()
 {
 
 }
@@ -93,12 +73,8 @@ void opcontrol()
   pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
   pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
-
   while (true)
   {
-    pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-                     (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-                     (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
     
     // Arcade control scheme
     int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
